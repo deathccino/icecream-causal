12896a0c58fc88031060a2b9ce748e6943969c6a
ccino <caiquefreitas2013@gmail.com>
2025-08-25 20:50:40 -0300

    transferring model logging to mlflow

--- src/load_and_process_data.py
+++ src/load_and_process_data.py
@@ -1,29 +1,18 @@
-#%%
 import numpy as np
 import pandas as pd
 import re
 from pathlib import Path
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
-# %%
-"""
-TO-DO functions:
-    load data;
-    feature engineering (dummy);
-    save processed data
-"""
-# %%
-def load_data_train(config: dict) -> pd.DataFrame:
-    file_path = config['data']['path']
-    df = pd.read_csv(file_path)
-    return df
+from utils import load_config
 
-def load_data_test(config: dict):
-    file_path = config['data']['path_rnd']
-    df = pd.read_csv(file_path)
-    return df
-# %%
-def create_dummies(df: pd.DataFrame, features):
+def load_data(filepath: str, filename: str) -> pd.DataFrame:
+    return pd.read_csv(Path(filepath) / filename)
+
+def save_df(df: pd.DataFrame, filepath: str, filename: str):
+    df.to_csv(Path(filepath) / filename, index=False)
+
+def fit_preprocessor(df: pd.DataFrame, features: list) -> ColumnTransformer:
     preprocessor = ColumnTransformer(
         transformers=[
             ('dummies', OneHotEncoder(sparse_output=False, dtype=int), features)
@@ -31,14 +20,34 @@ def create_dummies(df: pd.DataFrame, features):
         remainder='passthrough'
     ).fit(df)
 
-    cols = [re.sub('.*__', '', col) for col in preprocessor.get_feature_names_out()]
+    return preprocessor
+
+def create_dummies(df: pd.DataFrame, preprocessor: ColumnTransformer) -> pd.DataFrame:
+    prc = preprocessor
+    cols = [re.sub('.*__', '', col) for col in prc.get_feature_names_out()]
     df_prc = pd.DataFrame(
-        data=preprocessor.transform(df),
+        data=prc.transform(df),
         columns=cols
     )
 
     return df_prc
-# %%
-def save_df(config: dict, df: pd.DataFrame, filename: str):
-    filepath = Path(config['data']['processed']) / filename
-    df.to_csv(path_or_buf=filepath, index=False)
+
+def main():
+    #load the yaml configuration file
+    config = load_config()
+
+    filepath = config['data']['path']
+
+    #load and process train data
+    df_train = load_data(filepath + r'\\raw', 'ice_cream_train.csv')
+    prc = fit_preprocessor(df_train, ['weekday'])
+    save_df(create_dummies(df_train, prc), filepath + r'\\processed', filename='ice_cream_train.csv')
+    print("Train data processed and saved sucessfully")
+
+    #load and process test data
+    df_test = load_data(filepath + r'\\raw', 'ice_cream_test.csv')
+    save_df(create_dummies(df_test, prc), filepath + r'\\processed', filename='ice_cream_test.csv')
+    print("Test data processed and saved sucessfully")
+
+if __name__ == '__main__':
+    main()
--- src/model_training.py
+++ src/model_training.py
@@ -3,22 +3,62 @@ from sklearn.ensemble import GradientBoostingRegressor
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import mlflow
+from load_and_process_data import load_data
+from utils import load_config
+
+class ModelWrapper(mlflow.pyfunc.PythonModel):
+    def __init__(self, estimator: CausalForestDML):
+        self._estimator = estimator
+    
+    def get_estimator(self):
+        return self._estimator
+    
+    def predict(self, model_input: pd.DataFrame) -> np.ndarray:
+        return self._estimator.const_marginal_effect(model_input)
 
 def train_estimator(config: dict, df: pd.DataFrame):
 
     model_details = config['model']
 
-    m1 = CausalForestDML(
+    model = CausalForestDML(
         model_t=GradientBoostingRegressor(),
         model_y=GradientBoostingRegressor(),
         cv=3,
         random_state=1
     )
 
-    m1.fit(
+    model.fit(
         Y=df[model_details['outcome']],
         T=df[model_details['treatment']],
         X=df[model_details['features']]
     )
 
-    return m1
+    return model
+
+def log_mlflow_model(model, input_example):
+    model_pyfunc = ModelWrapper(model)
+    model_info = mlflow.pyfunc.log_model(
+        name='Causal-Forest',
+        python_model=model_pyfunc,
+        input_example=input_example
+    )
+    return model_info
+
+def main():
+    config = load_config()
+    filepath = config['data']['path'] + r'\\processed'
+
+    df_train = load_data(filepath , 'ice_cream_train.csv')
+    model = train_estimator(config, df_train)
+
+    mlflow.set_tracking_uri(config['mlflow']['uri'])
+    mlflow.set_experiment(experiment_name=config['mlflow']['experiment_name'])
+
+    with mlflow.start_run() as run:
+
+        #log the econml estimator
+        model_info = log_mlflow_model(model, input_example=df_train.loc[[0], :])
+
+if __name__ == '__main__':
+    main()
